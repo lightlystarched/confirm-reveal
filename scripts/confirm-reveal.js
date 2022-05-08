cf95b979a01c2a61ee7945f99f9a5e41e9e80b64
Joshua Drama <joshua.william.drama@gmail.com>
2022-05-08 09:07:07 -0400

    *fixed error in debug logger *hooked into onShowPlayers event

--- scripts/confirm-reveal.js
+++ scripts/confirm-reveal.js
@@ -1,8 +1,25 @@
 class ConfirmReveal {
   static ID = 'confirm-reveal';
 
+  static init() {
+    JournalSheet.prototype._onShowPlayers = ConfirmReveal.prototype._onShowPlayers;
+  }
+
+  // Rewrite _onShowPlayers method
+  async _onShowPlayers(event) {
+    ConfirmReveal.log(false, 'Confirming whether to show asset to players');
+    event.preventDefault();
+    await this.submit();
+
+    let permissions = this.object.data.permission;
+
+    ConfirmReveal.log(false, permissions);
+  }
+
+  // Debug logger - use with the Developer Mode module
+  // Syntax: ConfirmReveal.log(false, 'Message');
   static log(force, ...args) {
-    const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugVAlue(this.ID);
+    const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
 
     if (shouldLog) {
       console.log(this.ID, '|', ...args);
@@ -10,6 +27,8 @@ class ConfirmReveal {
   }
 }
 
+// Setup hooks for module
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
   registerPackageDebugFlag(ConfirmReveal.ID);
 });
+Hooks.on('init', ConfirmReveal.init);
